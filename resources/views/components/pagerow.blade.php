174b20d9b704e0e0b2d73d456a7599afd4c9a1df
hussam-aw <husam-hammad@hotmail.com>
2022-06-30 02:57:58 +0300

    fix repo errors 4

--- resources/views/components/pagerow.blade.php
+++ resources/views/components/pagerow.blade.php
@@ -1,66 +1,34 @@
 
 <div class="row bg-light justify-content-center align-items-center radius-80 mb-3 page-row " >
     <div class="col-md-2 col-12 ">
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
         <img class="rounded-circle" src="$page->image" alt="{{$page->name}}">
     </div>
     <div class="col-md-6 col-12">
-=======
->>>>>>> 294711eb82283337406eebec0217f88805b8f426
+
         <img class="rounded-circle" src="{{$page->image}}" alt="{{$page->name}}">
     </div>
-<<<<<<< HEAD
-    <div class="col-md-6 col-12">
-=======
     <div class="col-md-5 col-12">
->>>>>>> 02b5ef90dbc5d6998f22015d5ae8bc0d4ffc088b
         <div class="d-block  caption">
             <p class="blue-text my-1">{{$page->name}}</p>
             <a class="grey-text non-anchor"  href="{{'https://facebook.com/'.$page->page_id}}"><p>{{$page->page_id}}</p></a>
         </div>
     </div>
-<<<<<<< HEAD
+
     <div class="col-md-4 col-12 text-center">
 
 
-<<<<<<< HEAD
-=======
 
         @if ($page->status==0)
         <a class="btn mx-auto blue-bg  radius-20  white-text">تمكين</a>
->>>>>>> 294711eb82283337406eebec0217f88805b8f426
-=======
+
     <div class="col-md-5 col-12 text-center">
->>>>>>> 02b5ef90dbc5d6998f22015d5ae8bc0d4ffc088b
 
         @if ($page->status==0)
-
-<<<<<<< HEAD
-=======
->>>>>>> 02b5ef90dbc5d6998f22015d5ae8bc0d4ffc088b
->>>>>>> 294711eb82283337406eebec0217f88805b8f426
         <form action="{{route('enable_bot')}}" method="post">
             @csrf
             <input type="hidden" name="page_id" value="{{$page->page_id}}">
             <input type="hidden" name="token" value="{{ $page->token}}">
-<<<<<<< HEAD
-            <button type="submit" class="btn mx-auto blue-bg  radius-20  white-text">تمكين</button>
-        </form>
-        @else
-        <form action="{{route('disable_bot')}}" method="post">
-            @csrf
-            <input type="hidden" name="page_id" value="{{$page->page_id}}">
-            <input type="hidden" name="token" value="{{$page->token}}">
-            <button type="submit" class="btn btn-warning">تعطيل</button>
-        </form>
-        <a class="btn mx-auto green-bg  radius-20  white-text">تفعيل الرد</a>
-        @endif
-
-       {{--  <a href="get_posts/{{$page->page_id}}/{{$page->token}}" class="btn btn-success">posts</a> --}}
-    </div>
-=======
             <button type="submit" class="btn mx-auto blue-bg d-inline   radius-20  white-text">تمكين</button>
         </form>
         @else
@@ -78,9 +46,8 @@
                 <button type="submit" class="btn mx-auto green-bg d-inline radius-20  white-text">تفعيل الرد</button>
             </form>
         </div>
-       
+
         @endif
   </div>
->>>>>>> 02b5ef90dbc5d6998f22015d5ae8bc0d4ffc088b
 </div>
 
