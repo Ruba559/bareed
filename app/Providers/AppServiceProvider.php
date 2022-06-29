02ce119c458612d07c503f6728911ba7c17405b0
hussam-aw <husam-hammad@hotmail.com>
2022-06-30 02:51:22 +0300

    fix repo errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\Schema;
->>>>>>> 02b5ef90dbc5d6998f22015d5ae8bc0d4ffc088b
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,10 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-<<<<<<< HEAD
+
         //
-=======
+
         Schema::defaultStringLength(191);
->>>>>>> 02b5ef90dbc5d6998f22015d5ae8bc0d4ffc088b
+
     }
 }
